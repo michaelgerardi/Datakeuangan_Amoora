e3e133f8d031987c69db042d8f60347808236606
PutuSugosaAnggaramukti <74032914+PutuSugosaAnggaramukti@users.noreply.github.com>
2022-01-05 13:10:27 +0700

    login admin user

---
+++ app/Http/Controllers/Auth/authadmincontroller.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+
+use Auth;
+
+class authadmincontroller extends Controller
+{
+    public function showLoginForm()
+    {
+        if (Auth::guard('admin')->check()) {
+            return redirect()->route('homeadmin');
+        } else {
+            return view('auth.adminlogin');
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(auth()->guard('admin')->attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])) {
+            $user = auth()->user();
+
+            return redirect()->route('homeadmin');
+        } else {
+            return redirect()->route('admin.login')->withError('Credentials doesn\'t match.');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
